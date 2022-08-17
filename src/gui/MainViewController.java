8e6f5f4877d183b6e2dc34b28c1ca26a97571c08
Evandro <llpevandrodev@gmail.com>
2022-08-17 11:45:50 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,8 +17,9 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
@@ -26,55 +27,58 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
-	
+
 	@FXML
-	//com ação de inicialização
+	// com ação de inicialização
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->  {
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();
 		});
-		
+
 	}
-	
+
 	@FXML
 	// parte da inicialização, com exp. lambda
 	public void onMenuItemAboutAction() {
-		loadView ("/gui/About.fxml", x-> {});
+		loadView("/gui/About.fxml", x -> {
+		});
 	}
-		
+
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {		
+	public void initialize(URL url, ResourceBundle rb) {
 	}
-	
-	//função genérica T
+
+	// função genérica T
 	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVbox = loader.load();
-			
+
 			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent(); //getroot pega o primeiro elemento, no caso do mainview 
-			
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // getroot pega o primeiro elemento,
+																					// no caso do mainview
+
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVbox.getChildren());
-			
-			//essas duas linhas vão executar a inicialização departmentlist
+
+			// essas duas linhas vão executar a inicialização departmentlist
 			T controller = loader.getController();
 			initializingAction.accept(controller);
-			
-		}
-		catch (IOException e) {
+
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
 
 }
